b26c22d9d0819da4c47003b0fbeef18100b3bdda
Simon Weizman <simon@weizman.us>
2020-12-22 04:03:59 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "/verum-interactive-starmap" : "",
   images: {
     loader: "cloudinary",
     path: process.env.cloudinary_url,
